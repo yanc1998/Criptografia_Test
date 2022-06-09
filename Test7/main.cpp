4ab1789c19804fb25d4a6e77b6ab46dc256fe9b2
JCH97 <jc997hdez@gmail.com>
2022-06-09 11:29:33 -0400

    feat(Test7): add incomplete gamma function management

--- Test7/main.cpp
+++ Test7/main.cpp
@@ -2,23 +2,22 @@
 #include "iostream"
 #include <math.h>
 #include "vector"
+
 using namespace std;
 
-void printTable(vector<vector<int> > table)
+#define endl '\n'
+
+
+void printTable(vector<vector<int>> table)
 {
-    for (int i = 0; i < table.size(); i++)
-    {
+    for (int i = 0; i < table.size(); i++, cout << endl)
         for (int j = 0; j < table[i].size(); j++)
-        {
             cout << table[i][j] << ' ';
-        }
-        cout << '\n';
-    }
 }
 
-vector<vector<int> > fillTable(int N, int M, int sequence[])
+vector<vector<int>> fillTable(int N, int M, int sequence[])
 {
-    vector<vector<int> > table;
+    vector<vector<int>> table;
 
     int k = 0;
 
@@ -37,10 +36,9 @@ vector<vector<int> > fillTable(int N, int M, int sequence[])
 bool IsEqual(vector<int> block, int pos, int B[], int m)
 {
     for (int i = 0; i < m; i++)
-    {
         if (block[pos + i] != B[i])
             return false;
-    }
+
     return true;
 }
 
@@ -49,22 +47,52 @@ vector<int> fillWi(int m, vector<vector<int> > table, int B[])
     vector<int> Wi(table.size(), 0);
 
     for (int i = 0; i < table.size(); i++)
-    {
         for (int j = 0; j < table[i].size(); j++)
-        {
             if (IsEqual(table[i], j, B, m))
             {
                 Wi[i]++;
                 j += m - 1;
             }
-        }
-    }
+            
     return Wi;
 }
 
+
+double *gammaFunction(int maxm, double t) {
+    double s, term, *F;
+    double maxt = 20, eps = 1e-17;
+    int m;
+    
+    F = (double *)malloc((maxm + 1) * sizeof(double));
+    
+    if (t < maxt) {
+        term = 1.0 / (2 * maxm + 1);
+        s = term;
+        m = 1;
+        
+        while (fabs(term) > eps) {
+            term *= (2 * t) / (2 * maxm + 2 * m + 1);
+            s += term;
+            m++;
+        }
+        
+        F[maxm] = s * exp(-t);
+
+        for (m = maxm - 1; m >= 0; m--)
+            F[m] = (2 * t * F[m + 1] + exp(-t)) / (2 * m + 1);
+            
+    } else {
+        F[0] = 0.5 * sqrt(M_PI/t) * erf(sqrt(t));
+        for (m = 0; m <= maxm - 1; m++)
+            F[m + 1] = ((2 * m + 1) * F[m] - exp(-t)) / (2 * t);
+    }
+    return F;
+}
+
+
 float nonOverlappingTemplateMatching(int m, int n, int sequence[], int B[], int N, int M)
 {
-    vector<vector<int> > table = fillTable(N, M, sequence);
+    vector<vector<int>> table = fillTable(N, M, sequence);
     vector<int> Wi = fillWi(m, table, B);
 
     float mu = (M - m + 1) / pow(2, m);
@@ -73,11 +101,9 @@ float nonOverlappingTemplateMatching(int m, int n, int sequence[], int B[], int 
     float X2 = 0;
 
     for (int i = 0; i < N; i++)
-    {
         X2 += pow((Wi[i] - mu), 2) / (sigma2);
-    }
 
-    float Pvalue = 3; // llamar a la funcion igamac(N/2,X2/2)
+    float Pvalue = (float)(*gammaFunction(N / 2, (double)(X2 / 2)));
 
     printTable(table);
     printTable(vector<vector<int> >(1, Wi));
@@ -91,6 +117,8 @@ int main()
     int seq[20] = {1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0};
     int B[3] = {0, 0, 1};
     float Pvalue = nonOverlappingTemplateMatching(3, 20, seq, B, 2, 10);
-    cout << Pvalue << '\n';
+    
+    cout << Pvalue << endl;
+    
     return 0;
 }
